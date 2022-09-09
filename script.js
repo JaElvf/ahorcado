490803abc859e8b27b4471d9955de52cd0bb8999
Javier <ing.javier.vf@gmail.com>
2022-09-08 20:04:24 -0400

    Corregido las formulas de los gráficos y agregado el dibujo de letras en canvas

--- script.js
+++ script.js
@@ -14,9 +14,11 @@ function principal() {
    pantallaJuego.style.display = "block";
    pantallaInicio.style.display = "none";
    pantallaAgregarPalabra.style.display = "none";
+   jugar();
 }
 
 function dibujarLinea(ix,iy,fx,fy,color) {
+   pincel.beginPath();
    pincel.moveTo(ix,iy);
    pincel.lineTo(fx,fy);
    pincel.lineWidth = 6;
@@ -32,88 +34,94 @@ function dibujarCirculo(x, y, radio,color) {
    pincel.stroke();
 }
 
-function dibujarError(){
+function dibujarHorca(){
    let color = "#0A3871";
    switch (numeroErrores) {
       case 0:
          numeroErrores++;
          //suelo
-         dibujarLinea((ancho/2)-(ancho*0.3),(alto/2)+(alto*0.2),(ancho/2)+(ancho*0.3),(alto/2)+(alto*0.2),color);
+         dibujarLinea(ancho*0.2,alto*0.7,ancho*0.8,alto*0.7,color);
          break;
    
       case 1:
          numeroErrores++;
          //palo
-         dibujarLinea((ancho/2)-(ancho*0.2),(alto/2)-(alto*0.3),(ancho/2)-(ancho*0.2),(alto/2)+(alto*0.2),color);
+         dibujarLinea(ancho*0.3,alto*0.2,ancho*0.3,alto*0.7,color);
          break;
       case 2:
          numeroErrores++;
          //travesanho
-         dibujarLinea((ancho/2)-(ancho*0.2),(alto/2)-(alto*0.3),(ancho/2)+(ancho*0.2),(alto/2)-(alto*0.3),color);
+         dibujarLinea(ancho*0.3,alto*0.2,ancho*0.7,alto*0.2,color);
          break;
       case 3:
          numeroErrores++;
          //cuerda
-         dibujarLinea((ancho/2)+(ancho*0.2),(alto/2)-(alto*0.3),(ancho/2)+(ancho*0.2),(alto/2)-(alto*0.2),color);
+         dibujarLinea(ancho*0.7,alto*0.2,ancho*0.7,alto*0.3,color);
          break;
       case 4:
          numeroErrores++;
          //cabeza
-         dibujarCirculo((ancho/2)+(ancho*0.2),((alto/2)-(alto*0.2))+25,25,color);
+         dibujarCirculo(ancho*0.7,(alto*0.3)+25,25,color);
          break;
       case 5:
          numeroErrores++;
          //dorso
-         dibujarLinea((ancho/2)+(ancho*0.2),(alto/2)-(alto*0.2)+50,(ancho/2)+(ancho*0.2),(alto/2)-(alto*0.01),color);
+         dibujarLinea(ancho*0.7,(alto*0.3)+50,ancho*0.7,alto*0.49,color);
          break;
       case 6:
          numeroErrores++;
          //pierna derecha
-         dibujarLinea((ancho/2)+(ancho*0.2),(alto/2)-(alto*0.01),(ancho/2)+(ancho*0.1),(alto/2)+(alto*0.09),color);
+         dibujarLinea(ancho*0.7,alto*0.49,ancho*0.6,alto*0.59,color);
          break;
       case 7:
          numeroErrores++;
          //pierna izquierda
-         dibujarLinea((ancho/2)+(ancho*0.2),(alto/2)-(alto*0.01),(ancho/2)+(ancho*0.3),(alto/2)+(alto*0.09),color);
+         dibujarLinea(ancho*0.7,alto*0.49,ancho*0.8,alto*0.59,color);
          break;
       case 8:
          numeroErrores++;
          //pierna derecha
-         dibujarLinea((ancho/2)+(ancho*0.2),(alto/2)-(alto*0.2)+50,(ancho/2)+(ancho*0.1),(alto/2)-(alto*0.05),color);
+         dibujarLinea(ancho*0.7,(alto*0.3)+50,ancho*0.6,alto*0.45,color);
          break;
       case 9:
          numeroErrores++;
          //brazo izquierdo
-         dibujarLinea((ancho/2)+(ancho*0.2),(alto/2)-(alto*0.2)+50,(ancho/2)+(ancho*0.3),(alto/2)-(alto*0.05),color);
+         dibujarLinea(ancho*0.7,(alto*0.3)+50,ancho*0.8,alto*0.45,color);
          break;
    }
 }
 
 function dibujarEspacios(cantidad) {
-   let puntoInicial = 0.025;
-   let puntoFinal = 0.1;
-   let altura = alto-(alto*0.15);
-
+   let puntoInicial = 0.1;
+   let puntoFinal = puntoInicial + 0.08;
+   let altura = alto*0.84;
+   let color = "#0A3871";
    if(cantidad % 2 == 0){
-      for (let i = 1; i <= cantidad; i++) {
-         if(i <= cantidad / 2){
-            dibujarLinea((ancho/2)+(ancho*puntoInicial),altura,(ancho/2)+(ancho*puntoFinal),altura);
-            puntoInicial+=0.125;
-            puntoFinal+=0.125;
+      if(cantidad < 8){
+         for(let i = cantidad/2; i < 4;i++){
+            puntoInicial+=0.1;
          }
-         else{
-            if(i == (cantidad/2)+1){
-               puntoInicial = 0.025;
-               puntoFinal = 0.1;
-            }
-            dibujarLinea((ancho/2)-(ancho*puntoInicial),altura,(ancho/2)-(ancho*puntoFinal),altura);
-            puntoInicial+=0.125;
-            puntoFinal+=0.125;
-         }
+         puntoFinal = puntoInicial +0.08;
+      }
+      for(i=0; i < cantidad; i++){
+         dibujarLinea(ancho*puntoInicial, altura, ancho*puntoFinal, altura,color);
+         puntoInicial+= 0.1;
+         puntoFinal= puntoInicial+0.08;
       }
    }
    else{
-
+      puntoInicial = 0.05;
+      for(let i = cantidad/2; i < 4;i++){
+         puntoInicial+=0.1;
+      }
+      puntoFinal = puntoInicial +0.08;
+      console.log(puntoInicial);
+      console.log(puntoFinal);
+      for(i=0; i < cantidad; i++){
+         dibujarLinea(ancho*puntoInicial, altura, ancho*puntoFinal, altura,color);
+         puntoInicial+= 0.1;
+         puntoFinal= puntoInicial+0.08;
+      }
    }
    
 }
@@ -123,6 +131,23 @@ function dibujarLetra(letra,x,y,estilo,color) {
    pincel.font=estilo;
    pincel.fillText(letra,x,y);
 }
+
+function mensaje(estado) {
+   if(estado){
+      dibujarLetra("¡Felicidades ganaste!",ancho*0.35,alto*0.25,"1em 'Inter', sans-serif","green");
+   }
+   else{
+      dibujarLetra("¡Que mal, perdiste!",ancho*0.35,alto*0.25,"1em 'Inter', sans-serif","red");
+   }
+}
+
+function jugar(){
+   let estado = true;
+   dibujarHorca();
+   dibujarEspacios(7);
+   window.onkeydown = dibujarHorca;
+   mensaje(estado);
+}
 var pantallaInicio = document.getElementById("inicio");
 var pantallaAgregarPalabra = document.getElementById("agregarPalabra");
 var pantallaJuego = document.getElementById("juego");
@@ -131,19 +156,38 @@ var pincel = lienzo.getContext("2d");
 const ancho = lienzo.width;
 const alto = lienzo.height;
 var numeroErrores = 0;
-var posicionLetras = new Array();
+var start = false;
+if(start){
+   jugar();
+}
 
 //fondo
-pincel.fillStyle = "gray";
-pincel.fillRect(0,0,600,800);
-dibujarError();
-dibujarEspacios(8);
-window.onkeydown = dibujarError;
+//pincel.fillStyle = "gray";
+//pincel.fillRect(0,0,600,800);
 
-dibujarLetra("A",(ancho/2)-(ancho*0.090),alto-(alto*0.16),"3em 'Inter', sans-serif","#0A3871");
+//par
+/*
+dibujarLetra("A",ancho*0.1,alto*0.83,"4em 'Inter', sans-serif","#0A3871");
+dibujarLetra("B",ancho*0.2,alto*0.83,"4em 'Inter', sans-serif","#0A3871");
+dibujarLetra("C",ancho*0.3,alto*0.83,"4em 'Inter', sans-serif","#0A3871");
+dibujarLetra("B",ancho*0.4,alto*0.83,"4em 'Inter', sans-serif","#0A3871");
+dibujarLetra("E",ancho*0.5,alto*0.83,"4em 'Inter', sans-serif","#0A3871");
+dibujarLetra("F",ancho*0.6,alto*0.83,"4em 'Inter', sans-serif","#0A3871");
+dibujarLetra("Z",ancho*0.7,alto*0.83,"4em 'Inter', sans-serif","#0A3871");
+dibujarLetra("B",ancho*0.8,alto*0.83,"4em 'Inter', sans-serif","#0A3871");
+*/
 
-/*pincel.fillStyle = "#0A3871";
-pincel.font="3em 'Inter', sans-serif";
-pincel.fillText("B",(ancho/2)+(ancho*0.035),alto-(alto*0.16));
-pincel.fillText("A",(ancho/2)-(ancho*0.090),alto-(alto*0.16));
-pincel.fillText("A",(ancho/2)-(ancho*0.465),alto-(alto*0.16));*/
+//impar
+/*
+dibujarLetra("A",ancho*0.15,alto*0.83,"4em 'Inter', sans-serif","#0A3871");
+dibujarLetra("B",ancho*0.25,alto*0.83,"4em 'Inter', sans-serif","#0A3871");
+dibujarLetra("C",ancho*0.35,alto*0.83,"4em 'Inter', sans-serif","#0A3871");
+dibujarLetra("B",ancho*0.45,alto*0.83,"4em 'Inter', sans-serif","#0A3871");
+dibujarLetra("E",ancho*0.55,alto*0.83,"4em 'Inter', sans-serif","#0A3871");
+dibujarLetra("F",ancho*0.65,alto*0.83,"4em 'Inter', sans-serif","#0A3871");
+dibujarLetra("Z",ancho*0.75,alto*0.83,"4em 'Inter', sans-serif","#0A3871");
+*/
+
+
+//dibujarLinea(ancho/2,0,ancho/2,alto,"red");
+//dibujarLinea(0,alto/2,ancho,alto/2,"red");
